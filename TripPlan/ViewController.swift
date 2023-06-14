4cb1cbbe24eb47cc727f2e5268fd1507329231c6
Sena Küçükerdoğan <sena.kucukerdogan@gmail.com>
2023-06-14 19:02:20 +0300

    Trip Plan project created

---
+++ TripPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripPlan
+//
+//  Created by Sena Küçükerdoğan on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
